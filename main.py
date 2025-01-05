2b58c63fa7b19413781173ba1f14ce385137dc89
Shashank15DV <19.shashank.p@gmail.com>
2025-01-05 21:25:00 +0530

    few changes

--- main.py
+++ main.py
@@ -318,7 +318,7 @@ def getData():
 
 @app.route("/health")
 def health():
-    return jsonify({"success": true})
+    return jsonify({"success": True})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
